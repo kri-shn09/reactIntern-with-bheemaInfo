4f409802497ccdc405a7b9d31311d752c1a7c3a8
kri-shn09 <gupta.krishna7174@gmail.com>
2025-08-08 00:27:17 +0530

    modified files were added and commited

--- my-todo-list-app/src/App.jsx
+++ my-todo-list-app/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [userTask , setUserTask] = useState([]);
   const [inputValue , setInputValue] = useState('');
   const checkboxes = useRef([]);
+  const buttons = useRef([])
   
 const addTask = (evt)=>{
     evt.preventDefault(); 
@@ -18,10 +19,14 @@ const addTask = (evt)=>{
   }
 
   const firstCheck = (index)=>{
-     
     const box = checkboxes.current[index];              // get the box clicked
     box.classList.toggle('bg-green-600');        // toggle green class
-  
+  }
+
+  const deletTask = (ind)=>{
+   const some =  buttons.current[ind].parentElement;
+   const mainParent = some.parentElement;
+   mainParent.remove();
   }
  
   return (
@@ -50,13 +55,19 @@ const addTask = (evt)=>{
             onClick={()=> firstCheck(i)}
             className="w-6 cursor-pointer rounded-full
              h-6 border-3 border-gray-700 flex justify-center items-center">
-                  <i class="fas fa-check text-7 text-white "></i>
+                  <i className="fas fa-check text-7 text-white "></i>
             </div>
           </div>
           <div className="para-con">
             <p>{inp}</p>
           </div>
-        
+           <div className="cross-con">
+            <button
+            ref={(el) => buttons.current[i] = el}
+            onClick={()=> deletTask(i)}
+            className='font-extrabold text-red-600 text-3xl cursor-pointer'
+            >X</button>
+           </div>
         </div>
         )
        })
