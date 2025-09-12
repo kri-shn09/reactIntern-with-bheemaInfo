715d564fb0d4a7ac3e98399b81d5335f2ecc296c
kri-shn09 <gupta.krishna7174@gmail.com>
2025-09-12 21:38:30 +0530

    all files were modified and added

--- CareerBox-pro/src/components/LoginPage.jsx
+++ CareerBox-pro/src/components/LoginPage.jsx
@@ -7,10 +7,10 @@ function LoginPage() {
     className='w-full h-screen flex justify-center items-center main-con'
     >
         <form 
-        className='w-1/3 rounded-2xl px-8 py-5 relative bg-inherit'
+        className='w-1/3 rounded-2xl px-8 py-5 relative bg-white animate__animated animate__backInDown'
         autoComplete='off'
         >
-            <i className="fas fa-times text-xl text-white absolute top-5 right-6 cursor-pointer"></i>
+            <i className="fas fa-times text-xl absolute text-gray-500 top-5 right-6 cursor-pointer"></i>
             <h1
             className='font-bold text-2xl text-center my-3'
             >Log <span>in</span></h1>
@@ -19,22 +19,24 @@ function LoginPage() {
             className='text-white font-medium'
             htmlFor="email">Email :</label> <br />
             <input
-            className='w-full outline-0 border-2 border-blue-500 rounded-2xl px-3 py-1 text-white my-2'
+            className='w-full outline-0 border-2 border-blue-500 rounded-2xl pr-3 pl-8 py-1 text-blue-600 my-2'
             type="email" required name='email' placeholder='Users Email' /> <br /> 
 
             <label
             className='text-white font-medium'
             htmlFor="password">Password :</label> <br />
+            <i className="fas fa-eye text-gray-500 absolute top-49 right-11 eye-icon"></i>
             <input 
-             className='w-full outline-0 border-2 border-blue-500 rounded-2xl px-3 py-1 text-white my-2'
-            type="password" name="password" placeholder='Enter password' required />
+             className='w-full outline-0 border-2 border-blue-500 rounded-2xl px-3 py-1 text-blue-600 my-2 '
+            type="password" name="password" placeholder='Enter password' required  />
+            <i className="fas fa-envelope text-gray-500 absolute top-30 left-11"></i>
             
-            <div className="w-full flex justify-center items-center mt-3 flex-col">
+            <div className="w-full flex justify-center items-center mt-5 flex-col">
                 <button 
-                className='py-1 px-6 text-white bg-blue-500 rounded font-bold cursor-pointer'
+                className='py-2 px-9 text-white bg-blue-500 rounded font-bold cursor-pointer'
                 type="submit">Log In</button>
                 <p
-                className='mt-2 text-white font-medium '
+                className='mt-2  font-medium '
                 >Don't have an account ? <span className='s-link'>Sign up</span></p>
             </div>
         </form>
--- CareerBox-pro/src/components/SignupPage.jsx
+++ CareerBox-pro/src/components/SignupPage.jsx
@@ -9,57 +9,62 @@ function SignupPage() {
     >
       <form 
       autoComplete='off'
-      className='w-1/3 bg-inherit py-5 px-7 rounded-2xl animate__animated animate__backInDown relative'
+      className='w-1/3 bg-white py-5 px-7 rounded-2xl animate__animated animate__backInDown relative'
       >
-        <i className="fas fa-times absolute top-5 right-5 text-xl text-white cursor-pointer"></i>
+        <i className="fas fa-times absolute top-5 right-5 text-xl text-gray-400 cursor-pointer"></i>
         <h1
-        className='font-bold text-center text-3xl pb-5'
-        >Sign <span>up</span></h1>
+        className='font-bold text-center text-3xl'
+        >Sign up</h1>
 
         <label
-        className='font-medium text-white'
+        className='font-medium '
         htmlFor="fullName">
             Fullname :
             </label> <br />
+            <i className="fas fa-user cursor-pointer absolute top-25 left-10 text-gray-500 user-icon"></i>
         <input
-        className='my-4 w-full outline-0 py-1 pl-3 rounded-2xl border-2 border-blue-400 text-white'
+        className='my-2 w-full outline-0 py-1 pl-3 rounded-2xl border-2 border-blue-400 text-blue-600'
         type="text" name="fullName" placeholder='Enter Full Name' required  />
 
          <label
-        className='font-medium text-white'
+        className='font-medium '
         htmlFor="email">
             Email :
             </label> <br />
+            <i className="fas fa-envelope text-gray-500 top-44 left-10 absolute email-icon"></i>
         <input
-        className='my-4 w-full outline-0 py-1 pl-3 rounded-2xl border-2 border-blue-400 text-white'
+        className='my-2 w-full outline-0 py-1 pl-3 rounded-2xl border-2 border-blue-400 text-blue-600'
         type="email" name="email" placeholder='Enter your Email' required />
 
          <label
-        className='font-medium text-white'
+        className='font-medium '
         htmlFor="phone">
             Phone No. :
             </label> <br />
+            <i className="fas fa-phone-square-alt  text-gray-500 top-63 left-9 absolute phone-icon"></i>
         <input
-        className='my-4 w-full outline-0 py-1 pl-3 rounded-2xl border-2 border-blue-400 text-white'
+        className='my-2 w-full outline-0 py-1 pl-3 rounded-2xl border-2 border-blue-400 text-blue-600'
         type="phone" name="phone" placeholder='Enter Phone Number' required  />
 
          <label
-        className='font-medium text-white'
+        className='font-medium '
         htmlFor="password">
             Password :
             </label> <br />
+            <i className="fas fa-lock text-gray-500 top-81 left-10 absolute pass-icon"></i>
         <input
-        className='my-4 w-full outline-0 py-1 pl-3 rounded-2xl border-2 border-blue-400 text-white'
+        className='my-2 w-full outline-0 py-1 pl-3 rounded-2xl border-2 border-blue-400 text-blue-600'
         type="password" name="password" placeholder='Enter Password' required  />
+        <i className="fas fa-eye  text-gray-500 top-81 right-10 absolute cursor-pointer"></i>
 
-        <div className="w-full flex justify-center items-center">
+        <div className="w-full flex justify-center items-center flex-col">
            <button 
         type='submit'
         className='px-6 py-1 font-bold mt-5 rounded'
         >
           Sign up
         </button>
-
+        <p>Already have an account! <span>Login</span></p>
         </div>
 
         </form> 
